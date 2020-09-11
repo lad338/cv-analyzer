0fd26d8b000a08f781a195429d9ccb2fc27b4754
Dicky Li <dickyli338@gmail.com>
2020-09-11 18:08:22 +0800

    add multiple files tf-idf scanning

--- test/tfIdf.js
+++ test/tfIdf.js
@@ -1,14 +1,41 @@
 const tfIdf = require("../lib/tfIdf")
+const path = require('path');
+const fs = require("fs");
 
-const main = async () => {
-  const path = require('path');
+const test = async () => {
   const filePath = path.join(__dirname, "data/cv.pdf");
   const result = await tfIdf.getTermsFromPdfByPath(filePath);
   console.log(result);
 }
 
-main();
+const test2 = async () => {
+  const dirPath = path.join(__dirname, "data");
+  const result = await tfIdf.getTermsFromPdfsByPath(dirPath);
 
+  const json = JSON.stringify(result);
+
+  const resultFile = path.join(__dirname, "result/tfIdf.json")
+  fs.writeFile(resultFile, json, function(err) {
+    if (err) throw err;
+    console.log('complete');
+  });
+}
+
+const test3 = async () => {
+  const dirPath = path.join(__dirname, "data");
+  const {documents, tfidf} = await tfIdf.getTfIdfFromPdfsByPath(dirPath);
+  tfidf.tfidfs(
+    [
+      'java'
+    ],
+    function(i, measure) {
+    console.log(documents[i] + ' is ' + measure);
+  });
+}
+
+// test();
+// test2();
+test3();
 
 
 
